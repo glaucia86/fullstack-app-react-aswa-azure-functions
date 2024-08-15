91347b174c80ff7b05f061de2bbd93ead8d7d031
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-08-15 19:39:08 -0300

    feat: Add @heroicons/react as a dependency and update page header in App.tsx
    This commit adds @heroicons/react as a dependency in the package.json and package-lock.json files. It also updates the page header in the App.tsx file to include a check circle icon and align it with the TODO Application text.
    
    Refactor the code in the App.css file to include a new CSS class for the header container, which displays the check circle icon and the TODO Application text with a gap between them.
    
    These changes enhance the visual appearance and functionality of the TODO application.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,20 @@
+import { CheckCircleIcon } from '@heroicons/react/24/solid';
 import './App.css';
 
 function App() {
   return (
     <>
       <div>
-        <h1>TODO Application</h1>
+        <div className='header-container'>
+          <CheckCircleIcon className='logo' />
+          <h1>TODO Application</h1>
+        </div>
+
         <h2>with React + Azure Static Web Apps and Azure Functions</h2>
       </div>
       <div className='card'></div>
       <p className='read-the-docs'>
-        Made with ❤️ by{' JavaScript Advocacy Team at Microsoft '}
+        Made with ❤️ by JavaScript Advocacy Team at Microsoft
       </p>
     </>
   );
