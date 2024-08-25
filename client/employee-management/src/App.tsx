6ec3ce08156fd20e654b2c7061d00d3dda4fe4e2
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-08-25 17:02:28 -0300

    refactor: Update routing and navigation in App component

--- client/employee-management/src/App.tsx
+++ client/employee-management/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import ListEmployees from './pages/EmployeeList';
+import UpdateEmployee from './pages/UpdateEmployee';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className='min-h-screen bg-gray-100'>
+        <nav className='bg-blue-600 text-white p-4'>
+          <ul className='flex space-x-4'>
+            <li>
+              <Link to='/' className='hover:underline'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to='/list' className='hover:underline'>
+                Employee List
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className='container mx-auto p-4'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/list' element={<ListEmployees />} />
+            <Route path='/update/:id' element={<UpdateEmployee />} />
+          </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;
