30253f990e2b40de3f96ce6ddc1755e062242a86
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-10-31 21:57:36 -0300

    feat: Update employees function to return JSON response with timestamp

--- api/src/functions/employees.ts
+++ api/src/functions/employees.ts
@@ -1,11 +1,20 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { timeStamp } from "console";
 
 export async function employees(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
     const name = request.query.get('name') || await request.text() || 'world';
 
-    return { body: `Hello, ${name}!` };
+    return {
+        jsonBody: {
+            message: "Hello", timeStamp: new Date()
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        status: 200
+    }
 };
 
 app.http('employees', {
