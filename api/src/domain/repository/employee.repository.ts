d345f38d2dcc32ec0f0b5ac9ada174cd353aeba1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2025-01-28 21:16:44 -0300

    feat: add `IEmployeeRepository` interface for employee data operations

---
+++ api/src/domain/repository/employee.repository.ts
@@ -0,0 +1,10 @@
+import { Employee } from "../entities/employee.entity";
+
+export interface IEmployeeRepository {
+  findAll(): Promise<Employee[]>;
+  findById(id: string): Promise<Employee | null>;
+  findByRegistration(employeeRegistration: number): Promise<Employee | null>;
+  create(employee: Omit<Employee, 'id'>): Promise<Employee>;
+  update(id: string, employee: Partial<Omit<Employee, 'id' | 'createdAt' | 'employee_registration'>>): Promise<Employee>;
+  delete(id: string): Promise<void>;
+}
