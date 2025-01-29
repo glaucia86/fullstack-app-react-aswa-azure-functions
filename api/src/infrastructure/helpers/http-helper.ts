4274eb8500562b267467a60c151eeddc91f289d1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2025-01-28 22:47:01 -0300

    feat: add HTTP helper functions for standardized response handling

---
+++ api/src/infrastructure/helpers/http-helper.ts
@@ -0,0 +1,79 @@
+import { HttpResponseInit } from "@azure/functions";
+
+export function badRequest(message: string): HttpResponseInit {
+  return {
+    status: 400,
+    jsonBody: {
+      error: message
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+}
+
+export function notFound(message: string): HttpResponseInit {
+  return {
+    status: 404,
+    jsonBody: {
+      error: message
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+}
+
+export function serviceUnavailable(message: string): HttpResponseInit {
+  return {
+    status: 503,
+    jsonBody: {
+      error: message
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+}
+
+export function internalServerError(message: string): HttpResponseInit {
+  return {
+    status: 500,
+    jsonBody: {
+      error: message
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+}
+
+export function ok(body?: object): HttpResponseInit {
+  return body
+    ? {
+      status: 200,
+      jsonBody: body,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    : {
+      status: 204,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+}
+
+export function created(body: object): HttpResponseInit {
+  return {
+    status: 201,
+    jsonBody: body,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+}
+
+
+
